4300ec7d6212575fef42872b3e580f67a98eb92c
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2021-07-10 17:27:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#pip install gunicorn
+#pip freeze > requirements.txt
+#pip install -r requirements.txt
+#pip uninstall -r requirements.txt -y
+#pip3 install --index-url https://google-coral.github.io/py-repo/ tflite_runtime
---
+++ app.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from source.Predictor import Predictor
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+labels = ['Art Decor', 'Hi-Tech', 'IndoChinese', 'Industrial', 'Scandinavian']
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+class PredictBody(BaseModel):
+    url: str
+
+@app.post("/predict")
+async def predict(body: PredictBody):
+    url = body.url
+    if url is not None:
+        try:
+            import time
+            start = time.time()
+            output = Predictor.getInstance().ensemble_predict(image_url=url)
+            end = time.time() - start
+            print('time: ', end)
+        except():
+            return {
+                "success": False,
+                "message": "File not exist!"
+            }
+
+        return {
+            "success": True,
+            "message": "Predicted Results",
+            "result": (output*100).tolist(),
+            "Predicted time": str(round(end, 2)),
+            # "label": labels[int(np.argmax(output))],
+            # "score": np.max(output*100).tolist()
+        }
+    else:
+        return {
+            "success": False,
+            "message": "File not exist!"
+        }
